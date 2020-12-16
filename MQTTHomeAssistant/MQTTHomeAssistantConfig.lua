c6e2b3e102ca5282e88a2c5f24de008facaaaf49
ribenajuice <75055493+ribenajuice@users.noreply.github.com>
2020-12-16 11:01:11 +1030

    Create MQTTHomeAssistantConfig.lua

---
+++ MQTTHomeAssistant/MQTTHomeAssistantConfig.lua
@@ -0,0 +1,101 @@
+MQTT = {
+
+	LoungeBlinds = {
+    ['type'] = "cover",
+    ['MQTTName'] = 'Local/Lighting/Lounge Blinds',
+    ['ObjectID'] = '0/56/11',
+    ['Name'] = 'Lounge Blinds',
+    },
+    
+  Games = {
+    ['type'] = "light",
+    ['dimmable'] = true,
+    ['MQTTName'] = "Local/Lighting/Games Room",
+    ['ObjectID'] = "0/56/4",
+    ['Name'] = "Games Room",
+    },
+  Lounge = {
+    ['dimmable'] = true,
+    ['MQTTName'] = "Local/Lighting/Lounge",
+    ['ObjectID'] = "0/56/0",
+    ['Name'] = "Lounge",
+  	},
+  Kitchen = {
+    ['dimmable'] = true,
+    ['MQTTName'] = "Local/Lighting/Kitchen",
+    ['ObjectID'] = "0/56/5",
+    ['Name'] = "Kitchen",
+  	},
+  Entry = {
+    ['dimmable'] = true,
+    ['MQTTName'] = "Local/Lighting/Entry",
+    ['ObjectID'] = "0/56/34",
+    ['Name'] = "Entry",
+  	},
+  LoungeHallway = {
+    ['dimmable'] = true,
+    ['MQTTName'] = "Local/Lighting/Lounge Hallway",
+    ['ObjectID'] = "0/56/7",
+    ['Name'] = "Lounge Hallway",
+  	},
+  FrontPorch = {
+    ['dimmable'] = false,
+    ['MQTTName'] = "Local/Lighting/Front Porch",
+    ['ObjectID'] = "0/56/13",
+    ['Name'] = "Front Porch",
+  	},
+  FloodLights = {
+    ['dimmable'] = false,
+    ['MQTTName'] = "Local/Lighting/Flood Lights",
+    ['ObjectID'] = "0/56/14",
+    ['Name'] = "Flood Lights",
+  	},
+  BackYard = {
+    ['dimmable'] = false,
+    ['MQTTName'] = "Local/Lighting/Back Yard",
+    ['ObjectID'] = "0/56/25",
+    ['Name'] = "Back Yard",
+  	},
+  Patio = {
+    ['dimmable'] = false,
+    ['MQTTName'] = "Local/Lighting/Patio",
+    ['ObjectID'] = "0/56/6",
+    ['Name'] = "Patio",
+  	},
+  ClothesLine = {
+    ['dimmable'] = false,
+    ['MQTTName'] = "Local/Lighting/Clothes Line",
+    ['ObjectID'] = "0/56/28",
+    ['Name'] = "Clothes Line",
+  	},
+  Laundry = {
+    ['dimmable'] = false,
+    ['MQTTName'] = "Local/Lighting/Laundry",
+    ['ObjectID'] = "0/56/29",
+    ['Name'] = "Laundry",
+  	},
+  Toilet = {
+    ['dimmable'] = true,
+    ['MQTTName'] = "Local/Lighting/Toilet",
+    ['ObjectID'] = "0/56/2",
+    ['Name'] = "Toilet",
+  	},
+  Bathroom = {
+    ['dimmable'] = false,
+    ['MQTTName'] = "Local/Lighting/Bathroom",
+    ['ObjectID'] = "0/56/24",
+    ['Name'] = "Bathroom",
+  	},
+  Hallway = {
+    ['dimmable'] = true,
+    ['MQTTName'] = "Local/Lighting/Hallway",
+    ['ObjectID'] = "0/56/3",
+    ['Name'] = "Hallway",
+  	},
+  }
+
+MQTTDeviceName = "5500NAC"
+host = '10.0.0.17'
+port = 1883 
+username = 'user'
+password = 'testtest'
